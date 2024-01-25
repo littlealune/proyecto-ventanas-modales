2328c2b3e11112400ca847cd4d8757444b6a6102
SMarcoG <sara7915zuera@gmail.com>
2024-01-25 12:11:48 +0100

    Add open/close listeners
    Añadimos los listeners para que los modales se abran y se cierrem

--- script.js
+++ script.js
@@ -12,5 +12,10 @@ const closeModal = function () {
   modal.classList.add('hidden')
   overlay.classList.add('hidden')
 }
-  
-  
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal)
+
+btnCloseModal.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
+    
