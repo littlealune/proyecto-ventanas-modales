76bfb80c7da27edfaa4998650b76b72264eac402
SMarcoG <sara7915zuera@gmail.com>
2024-01-25 12:09:54 +0100

    Add functions
    Añadimos las funciones necesarias

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
 const btnsOpenModal = document.querySelectorAll('.show-modal')
+
+const openModal = function () {
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
+}
+    
+const closeModal = function () {
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+}
+  
+  
