f576e65a0d07d61d2f79692746f1a559d7829a5e
SMarcoG <sara7915zuera@gmail.com>
2024-02-02 16:35:22 +0100

    Add comments

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
+
+//declaración de variables y elementos
+
 const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
 const btnsOpenModal = document.querySelectorAll('.show-modal')
 
+//funcion de despliegue de ventana modal
+
 const openModal = function () {
   modal.classList.remove('hidden')
   overlay.classList.remove('hidden')
 }
-    
+
+//declaracion de función de cierre
+
 const closeModal = function () {
   modal.classList.add('hidden')
   overlay.classList.add('hidden')
 }
+
+//aplicación de las funciones anteriores
 
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal)
 
 btnCloseModal.addEventListener('click', closeModal)
 overlay.addEventListener('click', closeModal)
-    
+
 document.addEventListener('keydown', function (e) {
-// console.log(e, e.key);
+  // console.log(e, e.key);
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal()
   }
